28142c93927f1aee64a50d4f44b05ada6bd012be
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-12 14:59:47 +0900

    初期データを追加

--- postgres/init.sql
+++ postgres/init.sql
@@ -6,7 +6,11 @@ CREATE TABLE IF NOT EXISTS cypher.book(
     author     varchar(100) NOT NULL,
     publisher  varchar(100) NOT NULL,
     price      integer      NOT NULL,
-    created_at timestamp    NOT NULL,
-    updated_at timestamp    NOT NULL,
+    created_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (isbn)
 );
+
+INSERT INTO cypher.book VALUES
+('9784798142470', 'Spring徹底入門 Spring FrameworkによるJavaアプリケーション開発', '株式会社NTTデータ', '翔泳社', 4400),
+('9784297118594', 'Kotlinサーバーサイドプログラミング実践開発', '竹端尚人', '技術評論社', 3608);
