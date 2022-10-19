215e09f08148163617d0d05661d28f4c24645a20
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-19 17:35:39 +0900

    コメントを変更

--- cypher-book-api-web/build.gradle.kts
+++ cypher-book-api-web/build.gradle.kts
@@ -1,9 +1,6 @@
 dependencies {
     implementation(project(":cypher-book-api-application"))
     implementation(project(":cypher-book-api-domain"))
-
-    // CypherBookApiApplication が infra を DI できないので追加している
-    // Cypher では許容されているそう
-    // https://github.com/MxShun/cypher-book/pull/21#discussion_r744343872
+    // infra は web には依存しないが CypherBookApiApplication が web に存在するため依存関係に含める必要がある
     implementation(project(":cypher-book-api-infra"))
 }
