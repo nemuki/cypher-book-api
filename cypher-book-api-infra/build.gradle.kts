eaf371208a6d93851370fc4ba742d0c50a2fe546
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-21 12:09:35 +0900

    jdbc の依存関係を削除

--- cypher-book-api-infra/build.gradle.kts
+++ cypher-book-api-infra/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     implementation(project(":cypher-book-api-application"))
     implementation(project(":cypher-book-api-domain"))
 
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.2")
     runtimeOnly("org.postgresql:postgresql")
 }
