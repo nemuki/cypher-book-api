429cae73aef3a12d5dec2d75b09e50e4aad3f85f
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-24 15:52:31 +0900

    アノテーションを削除

--- cypher-book-api-infra/src/main/kotlin/dev/nemuki/cypherbookapi/infra/mapper/BookMapper.kt
+++ cypher-book-api-infra/src/main/kotlin/dev/nemuki/cypherbookapi/infra/mapper/BookMapper.kt
@@ -2,10 +2,9 @@ package dev.nemuki.cypherbookapi.infra.mapper
 
 import dev.nemuki.cypherbookapi.infra.entity.Book
 import org.apache.ibatis.annotations.Mapper
-import org.apache.ibatis.annotations.Param
 
 @Mapper
 interface BookMapper {
     fun selectAll(): List<Book>
-    fun findByIsbn(@Param("isbn") isbn: String): Book?
+    fun findByIsbn(isbn: String): Book?
 }
