428e868750cca575d2ecfda9272455c73b4801a8
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-24 15:47:06 +0900

    MyBatisのクエリをxmlへ

--- cypher-book-api-infra/src/main/kotlin/dev/nemuki/cypherbookapi/infra/mapper/BookMapper.kt
+++ cypher-book-api-infra/src/main/kotlin/dev/nemuki/cypherbookapi/infra/mapper/BookMapper.kt
@@ -3,21 +3,9 @@ package dev.nemuki.cypherbookapi.infra.mapper
 import dev.nemuki.cypherbookapi.infra.entity.Book
 import org.apache.ibatis.annotations.Mapper
 import org.apache.ibatis.annotations.Param
-import org.apache.ibatis.annotations.Select
 
 @Mapper
 interface BookMapper {
-    @Select(
-        """
-            SELECT * FROM cypher.book
-        """
-    )
     fun selectAll(): List<Book>
-
-    @Select(
-        """
-            SELECT * FROM cypher.book WHERE isbn = #{isbn}
-        """
-    )
     fun findByIsbn(@Param("isbn") isbn: String): Book?
 }
