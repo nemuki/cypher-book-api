e7fa28594d125b93171f4dbc7e8e47a107f6e39a
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-26 17:00:21 +0900

    12桁読み取るように

--- cypher-book-api-domain/src/main/kotlin/dev/nemuki/cypherbookapi/domain/entity/Isbn.kt
+++ cypher-book-api-domain/src/main/kotlin/dev/nemuki/cypherbookapi/domain/entity/Isbn.kt
@@ -7,11 +7,11 @@ data class Isbn(
 ) {
     init {
         // 1 -> 12 文字目までリスト化 → 足し算
-        val isbnSum = isbn.substring(0, 11).toList().mapIndexed { index, digit ->
+        val isbnSum = isbn.substring(0, 12).toList().mapIndexed { index, digit ->
             if (index % 2 == 0) {
-                digit.code
+                Character.getNumericValue(digit)
             } else {
-                digit.code * 3
+                Character.getNumericValue(digit) * 3
             }
         }.sum()
 
