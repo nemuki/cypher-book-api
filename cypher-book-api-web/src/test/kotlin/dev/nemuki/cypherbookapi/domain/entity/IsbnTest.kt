f1aa7be3e2fd9c6419a7f2a546a1dbfa765ab89f
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-10-31 10:10:21 +0900

    IsbnのチェックディジットのテストをParameterizedTestへ変更

--- cypher-book-api-web/src/test/kotlin/dev/nemuki/cypherbookapi/domain/entity/IsbnTest.kt
+++ cypher-book-api-web/src/test/kotlin/dev/nemuki/cypherbookapi/domain/entity/IsbnTest.kt
@@ -4,22 +4,30 @@ import dev.nemuki.cypherbookapi.domain.error.business.InvalidIsbnException
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class IsbnTest {
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("successCase")
     @DisplayName("正常系・ISBNのチェックディジットに成功する場合")
-    fun successIsbnCheckDigit() {
-        // 事前準備
-        val excepted = "9784798142470"
-
+    fun successIsbnCheckDigit(isbn: String) {
         // 実行
-        val actual = Isbn("9784798142470").isbn
+        val actual = Isbn(isbn).isbn
 
         // 検証
-        Assertions.assertEquals(excepted, actual)
+        Assertions.assertEquals(isbn, actual)
     }
+
+    fun successCase() = listOf(
+        Arguments.of("9784798142470"),
+        Arguments.of("9784297127473")
+    )
 
     @Test
     @DisplayName("異常系・ISBNのチェックディジットに失敗する場合")
