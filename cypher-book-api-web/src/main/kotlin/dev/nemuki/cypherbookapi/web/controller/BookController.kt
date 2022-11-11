3cd21a507cd0ca2537703dcf78ddea1d73c9687d
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-11-11 11:24:37 +0900

    InsertBookRequest のバリデーションを追加

--- cypher-book-api-web/src/main/kotlin/dev/nemuki/cypherbookapi/web/controller/BookController.kt
+++ cypher-book-api-web/src/main/kotlin/dev/nemuki/cypherbookapi/web/controller/BookController.kt
@@ -4,16 +4,18 @@ import dev.nemuki.cypherbookapi.application.usecase.FetchBook
 import dev.nemuki.cypherbookapi.application.usecase.InsertBook
 import dev.nemuki.cypherbookapi.domain.entity.Book
 import dev.nemuki.cypherbookapi.domain.entity.Isbn
+import dev.nemuki.cypherbookapi.domain.error.business.InvalidArgumentException
 import dev.nemuki.cypherbookapi.web.entity.BookResponse
 import dev.nemuki.cypherbookapi.web.entity.InsertBookRequest
 import dev.nemuki.cypherbookapi.web.entity.SuccessResponse
+import org.springframework.validation.BindingResult
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import javax.validation.Valid
 import javax.validation.constraints.Pattern
 
 @Validated
@@ -36,7 +38,19 @@ class BookController(
     }
 
     @PostMapping("/books")
-    fun insertBook(@Valid insertBookRequest: InsertBookRequest): SuccessResponse {
+    fun insertBook(
+        @RequestBody @Validated insertBookRequest: InsertBookRequest,
+        bindingResult: BindingResult,
+    ): SuccessResponse {
+        if (bindingResult.hasErrors()) {
+            val validationMessages = bindingResult.allErrors.map {
+                InvalidArgumentException.ValidationErrorMessage(
+                    field = it.objectName,
+                    description = it.defaultMessage
+                )
+            }
+            throw InvalidArgumentException(validationMessages)
+        }
         insertBook.insert(insertBookRequest.toEntity())
         return SuccessResponse("insert success")
     }
