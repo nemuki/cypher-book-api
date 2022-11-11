aa6c15a772eecc9d9e9f4fbf306cde53e92c18fc
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-11-11 13:02:34 +0900

    form-dataのリクエストに変更

--- cypher-book-api-web/src/main/kotlin/dev/nemuki/cypherbookapi/web/controller/BookController.kt
+++ cypher-book-api-web/src/main/kotlin/dev/nemuki/cypherbookapi/web/controller/BookController.kt
@@ -13,9 +13,9 @@ import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.validation.Valid
 import javax.validation.constraints.Pattern
 
 @Validated
@@ -39,14 +39,13 @@ class BookController(
 
     @PostMapping("/books")
     fun insertBook(
-        @RequestBody @Validated insertBookRequest: InsertBookRequest,
+        @Valid insertBookRequest: InsertBookRequest,
         bindingResult: BindingResult,
     ): SuccessResponse {
         if (bindingResult.hasErrors()) {
-
-            val validationMessages = bindingResult.fieldErrors.map {
+            val validationMessages = bindingResult.allErrors.map {
                 InvalidArgumentException.ValidationErrorMessage(
-                    field = it.field,
+                    field = it.objectName,
                     description = it.defaultMessage
                 )
             }
