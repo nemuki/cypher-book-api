496128b7e9fd8ff9fa2e63fd29fa738b51d67ae2
Naoki Takahashi <nemuki@users.noreply.github.com>
2022-11-11 11:49:54 +0900

    バリデーションを変更

--- cypher-book-api-web/src/main/kotlin/dev/nemuki/cypherbookapi/web/entity/InsertBookRequest.kt
+++ cypher-book-api-web/src/main/kotlin/dev/nemuki/cypherbookapi/web/entity/InsertBookRequest.kt
@@ -1,26 +1,31 @@
 package dev.nemuki.cypherbookapi.web.entity
 
-import org.springframework.lang.NonNull
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
 data class InsertBookRequest(
-    @field:NonNull
+    @get:NotEmpty
+    @field:NotNull
     @field:Pattern(regexp = "^[0-9]{13}$")
     val isbn: String,
 
-    @field:NonNull
+    @get:NotEmpty
+    @field:NotNull
     @field:Size(max = 100)
     val title: String,
 
-    @field:NonNull
+    @get:NotEmpty
+    @field:NotNull
     @field:Size(max = 100)
     val author: String,
 
-    @field:NonNull
+    @get:NotEmpty
+    @field:NotNull
     @field:Size(max = 100)
     val publisher: String,
 
-    @field:NonNull
+    @field:NotNull
     val price: Int,
 )
